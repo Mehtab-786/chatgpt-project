bd96d58998e8b4380606313d07b0a5d144cebd00
Mehtab Hussain <tradercoder786@gmail.com>
2025-08-29 09:49:48 +0530

    Cleaning up

--- chatgpt-backend/src/routes/chat.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const authMiddleware = require('../middlewares/auth.middlewares')
-const chatRouter = express.Router()
-const chatControllers = require('../controllers/chat.controllers')
-
-chatRouter.post('/', authMiddleware.authUser,chatControllers.createChat )
-
-module.exports = chatRouter
